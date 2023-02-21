a2b0e1e78b91cad2ba4fd502843335adf2356379
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-02-21 16:32:27 +0200

    update

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 import random
 import story
+from geopy import distance
 
 import mysql.connector
-from geopy import distance
 
 conn = mysql.connector.connect(
     host='localhost',
     port=3306,
-    database='demogame',
-    user='ilkkamtk',
-    password='q1w2e3r4',
+    database='some_database_name',
+    user='some_user_name',
+    password='some_password',
     autocommit=True
 )
 
@@ -18,11 +18,12 @@ conn = mysql.connector.connect(
 
 # select 30 airports for the game
 def get_airports():
-    sql = f'''SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
-             FROM airport
-             WHERE continent = 'EU' AND type='large_airport'
-             ORDER by RAND() 
-             LIMIT 30;'''
+    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
+FROM airport
+WHERE continent = 'EU' 
+AND type='large_airport'
+ORDER by RAND()
+LIMIT 30;"""
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -31,7 +32,7 @@ def get_airports():
 
 # get all goals
 def get_goals():
-    sql = f'''SELECT * FROM goal'''
+    sql = "SELECT * FROM goal;"
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -40,23 +41,24 @@ def get_goals():
 
 # create new game
 def create_game(start_money, p_range, cur_airport, p_name, a_ports):
-    sql = f'''INSERT INTO game (money, player_range, location, screen_name) VALUES (%s, %s, %s, %s)'''
+    sql = "INSERT INTO game (money, player_range, location, screen_name) VALUES (%s, %s, %s, %s);"
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql, (start_money, p_range, cur_airport, p_name))
     g_id = cursor.lastrowid
-    # add goals
+
+    # add goals / loot boxes
     goals = get_goals()
     goal_list = []
-    for g in goals:
-        for i in range(0, g['probability'], 1):
-            goal_list.append(g['id'])
+    for goal in goals:
+        for i in range(0, goal['probability'], 1):
+            goal_list.append(goal['id'])
 
-    # randomise airports, first make copy, exclude start
+    # exclude starting airport
     g_ports = a_ports[1:].copy()
     random.shuffle(g_ports)
 
     for i, goal_id in enumerate(goal_list):
-        sql = f'''INSERT INTO ports (game, airport, goal) VALUES (%s, %s, %s)'''
+        sql = "INSERT INTO ports (game, airport, goal) VALUES (%s, %s, %s);"
         cursor = conn.cursor(dictionary=True)
         cursor.execute(sql, (g_id, g_ports[i]['ident'], goal_id))
 
@@ -89,13 +91,6 @@ def check_goal(g_id, cur_airport):
     return result
 
 
-# update location
-def update_location(icao, p_range, u_money, g_id):
-    sql = f'''UPDATE game SET location = %s, player_range = %s, money = %s WHERE id = %s'''
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute(sql, (icao, p_range, u_money, g_id))
-
-
 # calculate distance between two airports
 def calculate_distance(current, target):
     start = get_airport_info(current)
@@ -114,6 +109,15 @@ def airports_in_range(icao, a_ports, p_range):
     return in_range
 
 
+# set loot box opened
+
+# update location
+def update_location(icao, p_range, u_money, g_id):
+    sql = f'''UPDATE game SET location = %s, player_range = %s, money = %s WHERE id = %s'''
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql, (icao, p_range, u_money, g_id))
+
+
 # game starts
 # ask to show the story
 storyDialog = input('Do you want to read the background story? (Y/N): ')
@@ -125,41 +129,41 @@ if storyDialog == 'Y':
 # GAME SETTINGS
 print('When you are ready to start, ')
 player = input('type player name: ')
-# boolean for game over
+# boolean for game over and win
 game_over = False
 win = False
+
 # start money = 1000
 money = 1000
 # start range in km = 2000
 player_range = 2000
+
 # score = 0
 score = 0
+
 # boolean for diamond found
 diamond_found = False
+
 # all airports
 all_airports = get_airports()
 # start_airport ident
 start_airport = all_airports[0]['ident']
+
 # current airport
 current_airport = start_airport
+
 # game id
 game_id = create_game(money, player_range, start_airport, player, all_airports)
 
-# GAME LOOP/FUNCTION
+# GAME LOOP
 while not game_over:
     # get current airport info
     airport = get_airport_info(current_airport)
     # show game status
     print(f'''You are at {airport['name']}.''')
     print(f'''You have {money:.0f}$ and {player_range:.0f}km of range.''')
-    # if diamond is found and player is at start, game is won
-    if diamond_found and current_airport == start_airport:
-        print(f'''You won! You have {money}$ and {player_range}km of range left.''')
-        win = True
-        game_over = True
-        break
     # pause
-    input("\033[32mPress Enter to continue...\033[0m")
+    input('\033[32mPress Enter to continue...\033[0m')
     # if airport has goal ask if player wants to open it
     # check goal type and add/subtract money accordingly
     goal = check_goal(game_id, current_airport)
@@ -176,13 +180,15 @@ while not game_over:
                 print(f'''Congratulations! You found {goal['name']}. That is worth {goal['money']}$.''')
                 print(f'''You have now {money:.0f}$''')
             elif goal['money'] == 0:
-                diamond_found = True
+                win = True
                 print(f'''Congratulations! You found the diamond. Now go to start.''')
             else:
                 money = 0
                 print(f'''Oh no! You have been robbed. You lost all your money''')
-        # pause
-        input("\033[32mPress Enter to continue...\033[0m")
+
+    # pause
+    input("\033[32mPress Enter to continue...\033[0m")
+
     # ask to buy fuel/range
     if money > 0:
         question2 = input('Do you want to by fuel? 1$ = 2km of range. Enter amount or press enter. ')
@@ -193,9 +199,10 @@ while not game_over:
             else:
                 player_range += question2 * 2
                 money -= question2
-                print(f'''You have now {money:.0f}$ and {player_range}km of range''')
+                print(f'''You have now {money:.0f}$ and {player_range:.0f}km of range''')
         # pause
         input("\033[32mPress Enter to continue...\033[0m")
+
     # if no range, game over
     # show airports in range. if none, game over
     airports = airports_in_range(current_airport, all_airports, player_range)
@@ -216,6 +223,11 @@ while not game_over:
         current_airport = dest
         if player_range < 0:
             game_over = True
+    # if diamond is found and player is at start, game is won
+    if win and current_airport == start_airport:
+        print(f'''You won! You have {money}$ and {player_range}km of range left.''')
+        game_over = True
+
 
 # if game is over loop stops
 # show game result
